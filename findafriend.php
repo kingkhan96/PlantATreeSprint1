3c67144d65a0ef8fc8e4e1d426d4ef0b1eb4fdbd
Shakaoaa <39017160+Shakaoaa@users.noreply.github.com>
2019-10-03 19:13:47 +1300

    Add files via upload

---
+++ findafriend.php
@@ -0,0 +1,40 @@
+<?php
+	$message = "";
+	if(isset($_POST['submit']))
+	{ //check if form was submitted
+		$username = $_POST['username'];
+		$message = "".$username." exists!";
+	}	
+?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="Styles.css" />
+		<title>Plant A Tree</title>
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<a href="Index.html"><img src="Images/Banner.jpg" width="956" height="130" alt="Banner" /></a>
+			</div><!--header-->
+			<div id="menu">
+				<a href="Index.html"><img src="Images/Home.jpg" width="140" height="180" alt="Home" /></a>
+				<a href="treesearch.html"><img src="Images/treesearch.jpg" width="140" height="180" alt="treesearch" /></a>
+				<a href="findafriend.html"><img src="Images/findafriend.jpg" width="140" height="180" alt="findafriend" /></a>
+				<a href="fill.html"><img src="Images/fill.jpg" width="140" height="180" alt="Top 10" /></a>
+			</div><!--menu-->
+			<div id="main">
+				<h1><u><b>Find a friend</b></u></h1>
+				<p>You can insert a username of someone you know and we will search if they are in our system.</p>
+				<?php echo $message; ?>
+				<form action = "searchstatusprocess.php" method ="get">
+					<h3>User: <input type="text" name="username"><br>
+					<input type="submit" name="submit" value="search"><br>
+				</form>
+				</div><!--main-->
+			<div id="footer">
+			<p>Software engineering 2019</p>
+			</div><!--footer-->
+		</div><!--container-->
+	</body>
+</html>
