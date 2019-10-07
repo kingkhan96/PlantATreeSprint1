1668cf56b733368d250cee853519a2efdde971dc
Shakaoaa <39017160+Shakaoaa@users.noreply.github.com>
2019-10-07 15:47:07 +1300

    Create register.php

---
+++ register.php
@@ -0,0 +1,38 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="Styles.css" />
+		<title>Plant A Tree</title>
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<a href="home.php"><img src="images/Banner.jpg" width="956" height="130" alt="Banner" /></a>
+			</div><!--header-->
+			<div id="menu">
+				<a href="home.php"><img src="images/Home.jpg" width="140" height="180" alt="Home" /></a>
+				<a href="treesearch.php"><img src="images/treesearch.jpg" width="140" height="180" alt="treesearch" /></a>
+				<a href="findafriend.php"><img src="images/findafriend.jpg" width="140" height="180" alt="findafriend" /></a>
+				<a href="fill.php"><img src="images/fill.jpg" width="140" height="180" alt="Top 10" /></a>
+			</div><!--menu-->
+			<div id="main">
+				<h1>Login</h1>
+				<form action = 'home.php' method ='post'>
+						<h3>Username: <input type='text' name='username'><br>
+						Password: <input type='text' name='password'><br>
+            Street line: <input type='text' name='street'><br>
+            City: <input type='text' name='city'><br>
+            Postcode: <input type='text' name='postcode'><br>
+            Phone: <input type='text' name='phone'><br></h3>
+						<input type='register' name='register'>
+						</form>
+						<a href='register.php'><p>register for an account here</p></a>
+					}
+				?>	
+			</div><!--main-->
+			<div id="footer">
+			<p>Software engineering 2019</p>
+			</div><!--footer-->
+		</div><!--container-->
+	</body>
+</html>
