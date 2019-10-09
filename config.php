5fe5af813aabe49c0d59a2396912f3bcafa7869b
Shakaoaa <39017160+Shakaoaa@users.noreply.github.com>
2019-10-10 12:52:46 +1300

    Update config.php

--- config.php
+++ config.php
@@ -7,13 +7,14 @@ $user_logged_in = false;
 try{
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
-} catch(Exception $e) {
+	$conn = new mysqli($server, $username, $password, $db);
+} catch(Exception $e) 
+{
 	echo "unable to connect".$e->getMessage();
 	exit;
 }
