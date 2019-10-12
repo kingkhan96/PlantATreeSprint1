cf317050b749f893af6732d80f6dbcb97d544aec
Shakaoaa <39017160+Shakaoaa@users.noreply.github.com>
2019-10-13 11:45:39 +1300

    making tables

--- config.php
+++ config.php
@@ -20,11 +20,28 @@ try{
 }
 // LOG IN
 try{
+	$table_make = "CREATE TABLE IF NOT EXISTS `users` (
+  			`id` INT(11) NOT NULL AUTO_INCREMENT,
+ 			`username` VARCHAR(60) NOT NULL,
+  			`description` VARCHAR(60) NOT NULL,
+  			`session_id` VARCHAR(255),
+  			PRIMARY KEY (`id`))";
+	mysqli_query($conn,$table_make);
+	
+	$table_make = "CREATE TABLE IF NOT EXIST `treedb` (
+  			`id` INT(11) NOT NULL AUTO_INCREMENT,
+ 			`tree` VARCHAR(60) NOT NULL,
+  			`price` int(60) NOT NULL,
+  			`growth` int(60) NOT NULL,
+  			`condition` VARCHAR(50) NOT NULL,
+  			`category` VARCHAR(50) NOT NULL,
+  			PRIMARY KEY (`id`))";
+	
 	$check_user = mysqli_query($conn,"SELECT * FROM users WHERE session_id = '".session_id()."' LIMIT 1");
 	while ($row=mysqli_fetch_row($check_user)){
 				if(count($row) > 0) {
 					$user_logged_in = true;
-					$_SESSION['user_id'] = $row[0];
+					$_SESSION['id'] = $row[0];
 					$_SESSION['username'] = $row[1];
 				}
 			}
