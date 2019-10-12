fa667bd7e43695658995082dec6c055b13d4d460
Shakaoaa <39017160+Shakaoaa@users.noreply.github.com>
2019-10-13 11:32:49 +1300

    adding sql queries

--- home.php
+++ home.php
@@ -1,3 +1,6 @@
+<?php
+include('config.php');
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -20,10 +23,32 @@
 				<h2>This is a website that is in testing that allows the purchase of trees through an account.</h2>
 				<?php
 					if(isset($_POST['submit']))
-					{ //check if form was submitted
-						$username = $_POST['username'];
-						$password = $_POST['password'];
-						echo "Login Failed, username/password is incorrect";
+					{ 
+						$user_results = mysqli_query($conn, "SELECT * FROM users WHERE username = '" .$logUsername. "' AND password = '" .$logPassword. "' LIMIT 1");
+						while ($row=mysqli_fetch_row($user_results))
+						{
+							if(count($row) > 0) 
+							{
+								$updateQuery = "
+									UPDATE users
+									SET session_id = '" .session_id(). "'
+									WHERE user_id = '" .$row[0]. "'";
+					
+									mysqli_query($conn,$updateQuery);
+									exit;
+							}
+							else
+							{
+								echo "<h1>Login</h1>";
+								echo " Your username and password don't seem to match.";
+								echo "<form action = '' method ='post'>";
+								echo "<h3>Username: <input type='text' name='username'><br>";
+								echo "Password: <input type='text' name='password'><br>";
+								echo "<input type='submit' name='submit'>";
+								echo "</form>" ;
+								echo "<a href='register.php'><p>register for an account here</p></a>";
+							}
+						}
 						
 					}
 					else
@@ -32,10 +57,6 @@
 						{
 							$username = $_POST['username'];
 							$password = $_POST['password'];	
-							$street = $_POST['street'];
-							$city = $_POST['city'];
-							$postcode = $_POST['postcode'];
-							$phone = $_POST['phone'];
 						echo "You have registered an account succesfully, please login below ".$username."<br>";
 						}
 						echo "<h1>Login</h1>";
